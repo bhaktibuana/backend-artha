93b22e906697aaf2da916cfb5e4dc27611f44e58
bhaktibuana <bhaktibuana19@gmail.com>
2023-07-19 01:43:35 +0700

    develop app logger and x-access-token validator

---
+++ src/routes/app/app.route.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const router = Router();
+
+/* [START ROUTING] */
+/* [END ROUTING] */
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,22 @@
 const { Router } = require("express");
+const { response, logFormat, logOptions } = require("../utils");
+const { xAccessTokenCheck } = require("../middlewares");
+const appRoute = require("./app/app.route");
+const morgan = require("morgan");
 
 const router = Router();
 
+router.use(morgan(logFormat, logOptions()));
+router.use("/api", xAccessTokenCheck, appRoute);
+
+router.use("/:anyRoute", (req, res) => {
+  const url = `${req.protocol}://${req.headers.host}${req.originalUrl}`;
+  response(`URL not found for: ${url}`, 404, res);
+});
+
 router.use("/", (req, res) => {
-  res.status(200).json({
-    message: "Artha API",
-  });
+  const url = `${req.protocol}://${req.headers.host}`;
+  response("Artha API", 200, res, { url });
 });
 
 module.exports = router;
